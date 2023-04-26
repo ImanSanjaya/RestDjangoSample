dd1e433cf7fd87c0694e5004b88221ef9062f0fb
Iman Sanjaya <imansanjaya800@gmail.com>
2023-04-26 19:41:57 +0700

    Sample Dasar Membuat Respon dari GET, POST dan PUT

--- core/home/views.py
+++ core/home/views.py
@@ -5,11 +5,22 @@ from rest_framework.response import Response
 # def hello_world(request):
 #     return Response({"message": "Hello, world!"})
 
-@api_view(['GET'])
+@api_view(['GET' , 'POST', 'PUT'])
 def index(request):
+
     courses = {
         'course_name' : 'Python',
         'learn' : ['Flask', 'Django', 'Tornado', 'FastApi'],
         'course_provider' : 'Sitektif Learning Center'
     }
-    return Response(courses)
+
+    if request.method == 'GET':
+        print('KAMU HIT GET METHOD')
+        return Response(courses)
+    elif request.method == 'POST':
+        print('KAMU HIT POST METHOD')
+        return Response(courses)
+    elif request.method == 'PUT':
+        print('KAMU HIT PUT METHOD')
+        return Response(courses)
+
